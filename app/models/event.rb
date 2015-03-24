34f25e8215425f3b7244f835752a8b5163c08259
Ahmed Owainati <aowainati2099@gmail.com>
2015-03-23 19:00:44 -0700

    Adding associations to character, event, and game models.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+  has_many :matches
+  has_many :games, through: :matches
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ActiveRecord::Base
+  has_many :characters
+  has_many :matches
+  has_many :events, through: :matches
 end
