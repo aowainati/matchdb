864be9009a61b9aace90235e62d207f7ad7a4eb2
Ahmed Owainati <aowainati2099@gmail.com>
2015-03-24 01:23:30 -0700

    Removing unnecessary specs that were just testing existing validations.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -2,16 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Character, :type => :model do
   it_behaves_like "it has a factory"
-
-  it "is invalid without a name" do
-    expect(build(:character, name: nil)).to_not be_valid
-  end
-
-  it "is invalid without a name of at least length 1" do
-    expect(build(:character, name: "")).to_not be_valid
-  end
-
-  it "is invalid without a game" do
-    expect(build(:character, game: nil)).to_not be_valid
-  end
 end
