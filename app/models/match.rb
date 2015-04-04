a966823ae4b5b67dcf6e215d0645ae447ea3740c
Ahmed Owainati <aowainati2099@gmail.com>
2015-04-03 23:07:40 -0700

    Added youtube_thumb helper to Match.

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,4 +4,8 @@ class Match < ActiveRecord::Base
   belongs_to :channel
 
   validates :title, :youtube_id, :game, :data, presence: true
+
+  def youtube_thumb
+    "http://i.ytimg.com/vi_webp/%s/mqdefault.webp" % youtube_id
+  end
 end
